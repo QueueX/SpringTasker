70b3763e54741019d1c1163e7ba72e5bfdf34819
Kinok0 <truthpruthbg@gmail.com>
2023-02-01 22:18:54 +0300

    0.3

--- src/main/java/com/exemple/springtasker/SpringTaskerApplication.java
+++ src/main/java/com/exemple/springtasker/SpringTaskerApplication.java
@@ -8,7 +8,7 @@ public class SpringTaskerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringTaskerApplication.class, args);
-        System.out.println("Application started");
+        System.out.println("Application started!");
     }
 
 }
